0249cff21ad40b575042ff3838de63c79d36dca5
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2021-05-24 14:08:05 +0300

    Добавляет решение задачи-5

--- js/task-05.js
+++ js/task-05.js
@@ -13,4 +13,36 @@
 // Индия - 80 кредитов
 // Ямайка - 120 кредитов
 
-const contry = prompt("Введите Вашу страну:");
+const destinationCountry = prompt("Введите Вашу страну:");
+const country1 = "Китай";
+const deliveryRate1 = 100;
+const country2 = "Чили";
+const deliveryRate2 = 250;
+const country3 = "Австралия";
+const deliveryRate3 = 170;
+const country4 = "Индия";
+const deliveryRate4 = 80;
+const country5 = "Ямайка";
+const deliveryRate5 = 120;
+let message;
+
+switch (destinationCountry.toLowerCase()) {
+  case country1.toLowerCase():
+    message = `Доставка в ${contry1} будет стоить ${deliveryRate1} кредитов`;
+    break;
+  case country2.toLowerCase():
+    message = `Доставка в ${country2} будет стоить ${deliveryRate2} кредитов`;
+    break;
+  case country3.toLowerCase():
+    message = `Доставка в ${country3} будет стоить ${deliveryRate3} кредитов`;
+    break;
+  case country4.toLowerCase():
+    message = `Доставка в ${country4} будет стоить ${deliveryRate4} кредитов`;
+    break;
+  case country5.toLowerCase():
+    message = `Доставка в ${country5} будет стоить ${deliveryRate5} кредитов`;
+    break;
+  default:
+    alert(`В вашей стране ${destinationCountry} доставка не доступна!`);
+}
+console.log(message);
